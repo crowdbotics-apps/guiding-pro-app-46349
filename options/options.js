a23461d1ba44eddfd6cfdf13fac09a0e0a2fd8b5
Crowdbotics <team@crowdbotics.com>
2024-02-14 17:22:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "guiding-pro-app-46349",
+  url: "https://guiding-pro-app-46349.botics.co",
+  api: "https://guiding-pro-app-46349.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
